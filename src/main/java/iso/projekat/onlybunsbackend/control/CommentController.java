87fd97d1bec04a3c3bf2475ab693fd899fa0c599
gozden2001 <gozdenovicboris@gmail.com>
2024-11-08 16:53:35 +0100

    testiranje commita

--- src/main/java/iso/projekat/onlybunsbackend/control/CommentController.java
+++ src/main/java/iso/projekat/onlybunsbackend/control/CommentController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RequestMapping("/api/comments")
 @AllArgsConstructor
 public class CommentController {
+
     private final CommentService commentService;
 
     @GetMapping("/post/{postId}")
