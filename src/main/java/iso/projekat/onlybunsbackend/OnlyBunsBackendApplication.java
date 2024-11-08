1673a27bb5c7c8f40e8f298fb16bf72828760e6b
Gibo2706 <bogdan.markovic2706@gmail.com>
2024-11-08 05:26:50 +0100

    initial commit

---
+++ src/main/java/iso/projekat/onlybunsbackend/OnlyBunsBackendApplication.java
@@ -0,0 +1,13 @@
+package iso.projekat.onlybunsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyBunsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyBunsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iso/projekat/onlybunsbackend/OnlyBunsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package iso.projekat.onlybunsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyBunsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
